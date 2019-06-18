6e2374da7f9d530215dbaf5b9d8572addad88fee
john <yongjun310@qq.com>
2019-06-18 14:54:29 +0800

    27

---
+++ app/src/main/java/com/leetcode/source/s1/S27.java
@@ -0,0 +1,21 @@
+package com.leetcode.source.s1;
+
+public class S27 {
+    public static int removeElement(int[] nums, int val) {
+        int res = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] != val) {
+                nums[res++] = nums[i];
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {3, 2, 2, 3};
+
+        int b = removeElement(a, 2);
+
+        System.out.print(" " + b);
+    }
+}
