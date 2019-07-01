7c7fb2594cac198d28ba5d35b5c0c4e9e9a2247d
john <yongjun310@qq.com>
2019-07-01 21:14:58 +0800

    s119

--- app/src/main/java/com/leetcode/source/s100/S119.java
+++ app/src/main/java/com/leetcode/source/s100/S119.java
@@ -1,5 +1,6 @@
 package com.leetcode.source.s100;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +12,9 @@ public class S119 {
             return  ret;
         }
         int half = (rowIndex-1)/2;
+        double fact = factorial(rowIndex);
         for(int i=0; i<=half+1; i++) {
-            ret.add(factorial(rowIndex)/(factorial(rowIndex-i)*factorial(i)));
+            ret.add((int)(Math.round(fact/(factorial(rowIndex-i)*factorial(i)))));
         }
         for(int i=half+1;i<rowIndex; i++) {
             ret.add(ret.get(rowIndex-1-i));
@@ -20,8 +22,8 @@ public class S119 {
         return ret;
     }
 
-    public static int factorial(int a) {
-        int result = 1;
+    public static double factorial(int a) {
+        double result = 1;
         for(int j=1;j<=a;j++) {
             result = result * j;
         }
@@ -30,7 +32,7 @@ public class S119 {
 
     public static void main(String[] args) {
 
-        List<Integer> b = getRow(0);
+        List<Integer> b = getRow(24);
         for(Integer j:b) {
             System.out.print(" " + j);
         }
