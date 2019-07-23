972d143736f168c6e6ee0292a30c4048c837ccfb
john <yongjun310@qq.com>
2019-07-23 19:57:32 +0800

    s9

---
+++ app/src/main/java/com/leetcode/source/s400/S434.java
@@ -0,0 +1,33 @@
+package com.leetcode.source.s400;
+
+/**
+ * Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
+ *
+ * Please note that the string does not contain any non-printable characters.
+ *
+ * Example:
+ *
+ * Input: "Hello, my name is John"
+ * Output: 5
+ */
+public class S434 {
+    public static int countSegments(String s)  {
+        if(s == null || "".equals(s)) {
+            return 0;
+        }
+        String[] seg = s.split(" ");
+        int count = 0;
+        for(String str:seg) {
+            if(!" ".equals(str) && str.length() > 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int b = countSegments(", , , ,        a, eaefa");
+
+        System.out.print(" " + b);
+    }
+}
