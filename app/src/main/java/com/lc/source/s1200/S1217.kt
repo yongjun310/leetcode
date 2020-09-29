fe249534f9c6202e13d22dd4f2d49463652eb563
john <yongjun310@qq.com>
2020-09-29 18:07:45 +0800

    s

---
+++ app/src/main/java/com/lc/source/s1200/S1217.kt
@@ -0,0 +1,74 @@
+import com.lc.source.s1100.S1160
+import java.util.*
+import kotlin.collections.HashMap
+
+/*
+1217. Minimum Cost to Move Chips to The Same Position
+Easy
+
+232
+
+934
+
+Add to List
+
+Share
+We have n chips, where the position of the ith chip is position[i].
+
+We need to move all the chips to the same position. In one step, we can change the position of the ith chip from position[i] to:
+
+position[i] + 2 or position[i] - 2 with cost = 0.
+position[i] + 1 or position[i] - 1 with cost = 1.
+Return the minimum cost needed to move all the chips to the same position.
+
+
+
+Example 1:
+
+
+Input: position = [1,2,3]
+Output: 1
+Explanation: First step: Move the chip at position 3 to position 1 with cost = 0.
+Second step: Move the chip at position 2 to position 1 with cost = 1.
+Total cost is 1.
+Example 2:
+
+
+Input: position = [2,2,2,3,3]
+Output: 2
+Explanation: We can move the two chips at poistion 3 to position 2. Each move has cost = 1. The total cost = 2.
+Example 3:
+
+Input: position = [1,1000000000]
+Output: 1
+
+
+Constraints:
+
+1 <= position.length <= 100
+1 <= position[i] <= 10^9
+Accepted
+22,202
+Submissions
+34,225
+*/
+class S1217{
+    fun minCostToMoveChips(position: IntArray): Int {
+        var c2 = 0
+        var c1 = 0
+        for(i in position.indices) {
+            if(position[i] % 2 == 0) {
+                c2++
+            } else {
+                c1++
+            }
+        }
+        return Math.min(c1, c2)
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+        }
+    }
+}
