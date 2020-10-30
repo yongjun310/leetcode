bc2f1f925f2914df4807e1b6d89307fba4494028
john <yongjun310@qq.com>
2020-10-30 18:38:51 +0800

    s

---
+++ app/src/main/java/com/lc/source/s500/S590.kt
@@ -0,0 +1,84 @@
+package com.lc.source.s500
+
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+import kotlin.collections.HashSet
+import kotlin.math.absoluteValue
+
+/*
+590. N-ary Tree Postorder Traversal
+Easy
+
+800
+
+71
+
+Add to List
+
+Share
+Given an n-ary tree, return the postorder traversal of its nodes' values.
+
+Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
+
+
+
+Follow up:
+
+Recursive solution is trivial, could you do it iteratively?
+
+
+
+Example 1:
+
+
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [5,6,3,2,4,1]
+Example 2:
+
+
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [2,6,14,11,7,3,12,8,4,13,9,10,5,1]
+
+
+Constraints:
+
+The height of the n-ary tree is less than or equal to 1000
+The total number of nodes is between [0, 10^4]
+Accepted
+108,044
+Submissions
+148,532
+ */
+
+class S590{
+    class Node(var `val`: Int) {
+        var children: List<Node?> = listOf()
+    }
+
+    fun postorder(root: Node, list:ArrayList<Int>) {
+        for(e in root.children) {
+            if(e != null) {
+                postorder(e, list)
+            }
+        }
+        list.add(root.`val`)
+    }
+
+    fun postorder(root: Node?): List<Int> {
+        var list = ArrayList<Int>()
+        if(root == null) {
+            return list
+        }
+        postorder(root, list)
+        return list
+    }
+
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>) {
+        }
+    }
+}
